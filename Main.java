d8e0ef7f2e33432d1245ecc5907252494436e645
alexandraterchea <161625295+alexandraterchea@users.noreply.github.com>
2025-02-27 17:21:30 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
+       Participant p1=new Participant("Paul",20,"12345678","p@yahoo.com");
+        Participant p2=new Participant("Alexandra",21,"123456782000","ale@yahoo.com");
+        adaugaParticipant(p1);
+        adaugaParticipant(p2);
+        TurAventura tur1= new TurAventura("nume1","ghid1",,200,50);
+        TurAventura tur2= new TurAventura("nume2","ghid2",,200,50);
+        TurAventura tur3= new TurAventura("nume3","ghid3",,200,50);
 
-        System.out.printf("Hello and welcome!");
+        TurCulturale tur4=new TurCulturale("nume4","ghid4",,200,50);
+        TurCulturale tur5=new TurCulturale("nume5","ghid5",,200,50);
+        TurCulturale tur6=new TurCulturale("nume6","ghid6",,200,50);
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
     }
 }
