dd6286eb890b0f362a49d4c3ae5b2f1cd68fd835
alexandraterchea <161625295+alexandraterchea@users.noreply.github.com>
2025-02-27 17:22:33 +0200

    Update TurAventura.java

--- TurAventura.java
+++ TurAventura.java
@@ -1,3 +1,32 @@
-class TurAventura extends Tur {
+import java.util.List;
 
+class TurAventura extends Tur {
+      private double _price;
+      private double _cost_km;
+    TurAventura(String nume, String ghid, List<Participant> participanti,double price,double cost_km)
+    {
+        setName(nume);
+        setGhid(ghid);
+        setPrice(price);
+        setCostKm(cost_km);
+
+    }
+
+     public double getPrice()
+     {
+         return _price;
+     }
+     public double getCostKm()
+     {
+         return _cost_km;
+     }
+     void setPrice(double price)
+     {
+         _price=price;
+
+     }
+     void setCostKm(double cost_km)
+     {
+         _cost_km=cost_km;
+     }
 }
