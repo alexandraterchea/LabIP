e877d3a01d10c39285acc7de4d8e97da04acf3f0
alexandraterchea <161625295+alexandraterchea@users.noreply.github.com>
2025-02-27 17:22:49 +0200

    Update TurCulturale.java

--- TurCulturale.java
+++ TurCulturale.java
@@ -1,5 +1,48 @@
-class TurCulturale extends Tur {
-    void showDetails() {
+import java.util.List;
 
+class TurCulturale extends Tur {
+
+    private double _price;
+    private double _cost_km;
+    private double _discount;
+    TurCulturale(String nume, String ghid, List<Participant> participanti, double price, double cost_km,double discount)
+    {
+        setName(nume);
+        setGhid(ghid);
+        setPrice(price);
+        setCostKm(cost_km);
+        setDiscount(discount);
+
+    }
+
+
+    public double getPrice()
+    {
+        return _price;
+    }
+    public double getCostKm()
+    {
+        return _cost_km;
+    }
+    void setPrice(double price)
+    {
+        _price=price;
+
+    }
+    void setCostKm(double cost_km)
+    {
+        _cost_km=cost_km;
+    }
+    private double getTotal( )
+    {
+
+    }
+    void setDiscount(double discount)
+    {
+        _discount=discount;
+    }
+    public double getDiscount()
+    {
+        return _discount;
     }
 }
